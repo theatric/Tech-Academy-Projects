9abca20f333f450d47ae447026b94b5520006ce9
theatric <78820620+theatric@users.noreply.github.com>
2024-02-20 20:19:40 +0000

    completion of Project1_Expressions_Alert

---
+++ JavaScript-Projects/Basic-JavaScript-Projects/Project1_Expressions_Alert/js/main.js
@@ -0,0 +1,36 @@
+// window.alert() Method Assignment
+window.alert("Hello, World!");
+
+// document.write() Method Assignment
+document.write("Hello, World!<br>");
+
+// Variable Assignment
+var A = "This is a string.<br>"
+document.write(A);
+
+// String Assignment
+var B = "This is string alert."
+window.alert(B);
+
+// Escape Character Assignment
+var C = "This string\'s apostrophe and inverted commas have been \'escaped\'.<br>"
+document.write(C);
+
+// Concatenation Assignment
+document.write("You wouldn\'t know " + "that " + "this is a " + "concatenated " + "string.<br>");
+
+// Concatenated String Challenge
+var D = "Another " + "concatenated " + "string.<br>"
+document.write(D);
+
+// Multiple Variables Assignment
+var Family = "Jones", Father = "Tom", Mother = "Martha", Son = "Nathan", Daughter = "Poppy";
+document.write(Son + " is the youngest member of the " + Family + " family.<br>");
+
+// Expression Assignment
+document.write(37 + 3 + "<br>");
+
+// Basic JavaScript Projects Assignment
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string.";
+document.write(Sent1 + Sent2 + "<br><br>");
