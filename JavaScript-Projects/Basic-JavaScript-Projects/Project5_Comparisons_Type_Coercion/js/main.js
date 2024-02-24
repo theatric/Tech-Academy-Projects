64818fd15ad61489be8b8d1272f5994a26aac2aa
theatric <78820620+theatric@users.noreply.github.com>
2024-02-24 11:09:04 +0000

    Some minor corrections

---
+++ JavaScript-Projects/Basic-JavaScript-Projects/Project5_Comparisons_Type_Coercion/js/main.js
@@ -0,0 +1,20 @@
+// Type Of Operator Assignment
+document.write(typeof "leopard");
+
+// Type Coercion Assignment
+document.write("<br>" + "300" + 6);
+
+// NaN Challenge
+document.write("<br>" + 0/0);
+document.write("<br>" + isNaN("This is a string."));
+document.write("<br>" + isNaN(23));
+
+// Infinity Assignment
+document.write("<br>" + 1E310);
+document.write("<br>" + -1E310);
+
+// Boolean Assignment
+document.write("<br>" + (10 > 3));
+document.write("<br>" + (10 < 3));
+
+console.log(2 + "33");
