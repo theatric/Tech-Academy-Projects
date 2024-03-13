dad9023d8f6f73b23956792f59ddfe0a690fea8e
theatric <78820620+theatric@users.noreply.github.com>
2024-03-13 21:31:47 +0000

    jQuery Assignment

---
+++ JavaScript-Projects/JavaScript-Projects/js/JavaScript_4.js
@@ -0,0 +1,7 @@
+
+// Elements Challenge
+const doctypeObj = document.doctype;
+document.getElementById("demo").innerHTML = doctypeObj.name;
+
+const encoding = document.characterSet;
+document.getElementById("demo2").innerHTML = encoding;
